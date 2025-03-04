a3919f4aa987b32388cd0afb8572c8d026e9fd19
AzizaTurgunboyeva <turgunboyevaa119@gmail.com>
2025-03-04 15:52:40 +0500

    added test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe(`Hello World!PORT = ${process.env.PORT}`);
+      expect(appController.getHello()).toBe(`Hello World!PORT = 3003}`);
     });
   });
 });
